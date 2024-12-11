f3c9adedb9a9112d50ee7dbabde24b902748167e
Tymoteusz <tymoteusz.majewski1@gmail.com>
2024-12-11 22:08:38 +0100

    feat: implemented ToString() method for Product object

--- APN/Product.cs
+++ APN/Product.cs
@@ -4,4 +4,6 @@ public class Product(string productName, decimal price)
 {
     public string ProductName { get; set; } = productName;
     public decimal Price { get; set; } = price;
+    
+    public override string ToString() => $"{ProductName}: {Price}zł";
 }
